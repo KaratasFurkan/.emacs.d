e346e3fb2ef8c2d8ff3f2f41197c4e3cb311071d
Furkan Karataş <furkan.karatas02@gmail.com>
2020-01-09 18:07:15 +0300

    Load tangled .el file if exists to decrease startup time

--- init.el
+++ init.el
@@ -1,1 +1,6 @@
-(setq ring-bell-function 'ignore)
+(defvar config-org (expand-file-name "README.org" user-emacs-directory))
+(defvar config-el (expand-file-name "README.el" user-emacs-directory))
+
+(if (file-exists-p config-el)
+    (load-file config-el)
+  (org-babel-load-file config-org))
