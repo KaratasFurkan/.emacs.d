d8f4786e379f29fc7f9545a82d68749ce6deaaab
Furkan Karataş <furkan.karatas02@gmail.com>
2020-07-22 01:21:36 +0300

    Require org for "org-babel-tangle-file"

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (defconst config-org (expand-file-name "README.org" user-emacs-directory))
 (defconst config-el (expand-file-name "config.el" user-emacs-directory))
 
-(if (file-exists-p config-el)
-    (load-file config-el)
+(unless (file-exists-p config-el)
+  (require 'org)
   (org-babel-tangle-file config-org config-el))
+
+(load-file config-el)
